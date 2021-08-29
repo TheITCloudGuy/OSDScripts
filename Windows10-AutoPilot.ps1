bc1f426ad23cdfd44518d2fe388bee6b18501688
devliano <43003164+devliano@users.noreply.github.com>
2021-08-29 20:13:06 +0100

    Update Windows10-AutoPilot.ps1

--- Windows10-AutoPilot.ps1
+++ Windows10-AutoPilot.ps1
@@ -1,3 +1,4 @@
+Install-Module OSDProgress
 Watch-OSDCloudProvisioning {
     Write-Host -ForegroundColor Cyan "Windows 10 - AutoPilot Zero Touch"
 
@@ -5,17 +6,23 @@ Watch-OSDCloudProvisioning {
     Update-OSDProgress -Text "Starting AutoPilot Pre-Installation checks." # output to UI
     Write-Host  -ForegroundColor Cyan "Starting AutoPilot Pre-Installation checks." # output to console
     Start-Sleep -Seconds 3
-    Update-OSDProgress -Text "Downloading and Installing required Modules"
+
+    Update-OSDProgress -Text "Downloading and Installing required Modules"#
     Install-Module OSD -Force
     Import-Module OSD -Force
+    Install-Module AutopilotOOBE
     Update-OSDProgress -Text " " # hide first text
 
     Start-OSDCloud -OSBuild 21H1 -OSEdition Education -ZTI
-
     #Anything I want  can go right here and I can change it at any time since it is in the Cloud!!!!!
     Update-OSDProgress -Text "Starting OSDCloud PostAction stuff..."
     Write-Host  -ForegroundColor Cyan "Starting OSDCloud PostAction stuff..."
     Start-Sleep -Seconds 5
+    Update-OSDProgress -Text " " # hide first text
+
+    Update-OSDProgress -Text "Enrolling in AutoPilot MDM"
+    Write-Host  -ForegroundColor Cyan "Enrolling in AutoPilot MDM"
+    Start-AutopilotOOBE
     Update-OSDProgress -Text " " # hide first text
     
     # lets throw an error, just for fun
